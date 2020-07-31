e969be5a4b6810c6b4aeea4e19a2a7436d694359
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-07-30 21:10:20 -0300

    Testes de rota de formulário de lista

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,4 +2,7 @@ class ListController < DashboardController
   def index
     @lists = current_user.lists
   end
+
+  def new
+  end
 end
--- spec/controllers/list_controller_spec.rb
+++ spec/controllers/list_controller_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe ListController do
       expect(controller.instance_variable_get(:@lists)).to eq([])
     end
   end
+
+  describe "GET new" do
+    it "redirects when user is not authenticated" do
+      get :new
+      assert_redirected_to new_user_session_path
+    end
+
+    it "is ok when user is authenticated" do
+      user = double('Petris', :lists => [])
+      
+      sign_in user
+      get :new
+      
+      assert_response 200
+    end
+  end
 end
