0ff37963b46a6a822e422b6587a157c3073f0e4f
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-02 17:34:14 -0300

    Redireciona root url para url de listas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def redirect_to_lists
+    redirect_to lists_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations', sessions: 'sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  root 'list#index' 
+  root 'application#redirect_to_lists' 
   get '/lists', to: 'list#index'
   get '/lists/new', to: 'list#new'
   post '/lists/new', to: 'list#create'
