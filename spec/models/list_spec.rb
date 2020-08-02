5593757e742feb143cf4c2299fbbc49817579d15
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-02 17:45:12 -0300

    Specs de list criado

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe List do
+  it "is valid with attributes"
+
+  it "is not valid with empty title"
+
+  it "is not valid with visibility different than boolean values"
+
+  it "is not valid if title length is greater than 100 characters"
+
+  it "can be closed"
+
+  it "has tasks associated"
+end
