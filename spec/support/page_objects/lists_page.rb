6e7e1db1a27d762bac2ab38a259d1984515c41bf
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-03 02:34:10 -0300

    Criado container para listas criadas

--- spec/support/page_objects/lists_page.rb
+++ spec/support/page_objects/lists_page.rb
@@ -1,2 +1,15 @@
 class ListsPage < BasePage
+  def visit_page
+    visit lists_path
+
+    self
+  end
+
+  def close_first_list
+    within all('.cards-container').first do
+      within all('.card').first do
+        #click_button "Close"
+      end
+    end
+  end
 end
