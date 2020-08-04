c25bb3fe575bfa0644b42acddd4768a4d7fd7090
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-04 06:36:00 -0300

    Verifica se lista é favoritável: não pertencendo ao usuário logado e não estando com visibilidade privada

--- app/controllers/bookmark_list_controller.rb
+++ app/controllers/bookmark_list_controller.rb
@@ -4,6 +4,27 @@ class BookmarkListController < ApplicationController
   end
 
   def new
+    return redirect_to :back if self.available_for_bookmark?
+   
     current_user.bookmarks.create({list_id: params[:id]})
+
+    redirect_to lists_public_path
+  end
+
+  protected 
+
+  def available_for_bookmark?
+    list_is_bookmarkable? == false && list_already_bookmarked? == false
+  end
+  
+  def list_is_bookmarkable?
+    List.not_belonging_to_user(current_user)
+        .visible_for_users
+        .where(id: params[:id])
+  end
+
+  def list_already_bookmarked?
+    Bookmark.from_user(current_user)
+            .where(id: params[:id])
   end
 end
