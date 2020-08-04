aff3d42171727736955888dc49ad01582b8a44c9
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-04 01:38:10 -0300

    Valida atributos de tarefa

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task do
-  let(:valid_params) {{list_id: 1, title: "title"}}
+  let(:valid_params) {{list_id: 1, description: "title"}}
 
-  fixtures :task
+  fixtures :lists
 
-  it "is valid with attributes"
+  it "is valid with attributes" do
+    expect(Task.create(valid_params).valid?).to be true
+  end
+
   it "is created with status open"  
   it "is not valid with empty title"
   it "is not valid if title length is greater than 100 characters"
