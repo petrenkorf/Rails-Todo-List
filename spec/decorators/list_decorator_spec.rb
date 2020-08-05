7abfbba0aea621ecc0b3927df0b7a018f74eb82e
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-05 02:42:01 -0300

    Criação de specs de ListDecorator

---
+++ spec/decorators/list_decorator_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ListDecorator do
+  it "is instantiated with a list as argument"
+  it "delegates method calls to list argument"
+  it "formats list title"
+  it "broadcasts message to actioncable"
+end
