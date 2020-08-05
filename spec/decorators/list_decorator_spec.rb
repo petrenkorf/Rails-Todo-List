40ed78ce05e8fd1219fb8f94d22267cdbb0234e7
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-05 02:46:13 -0300

    Testa se chamadas do decorator estão sendo delegadas para lista

--- spec/decorators/list_decorator_spec.rb
+++ spec/decorators/list_decorator_spec.rb
@@ -6,7 +6,16 @@ RSpec.describe ListDecorator do
 
     expect(ListDecorator.new(list)).to be_truthy
   end
-  it "delegates method calls to list argument"
+
+  it "delegates method calls to list argument" do
+    list = double(list)
+    allow(list).to receive(:close)
+
+    ListDecorator.new(list).close
+
+    expect(list).to have_received(:close)
+  end
+
   it "formats list title"
   it "broadcasts message to actioncable"
 end
