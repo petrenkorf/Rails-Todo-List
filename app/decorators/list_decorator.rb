fcf9cffc0a9ad09594f263fd5a6846f93df134f2
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-05 03:05:31 -0300

    Testes de broadcast via actioncable

--- app/decorators/list_decorator.rb
+++ app/decorators/list_decorator.rb
@@ -7,4 +7,14 @@ class ListDecorator < SimpleDelegator
   def message
     "A lista de tarefas \"#{@list.title}\" foi atualizada."
   end
+
+  def close
+    list = @list.close
+
+    list.bookmark_user_ids.each do |id| 
+      ActionCable.server.broadcast "list_update_notifier_channel_#{id}", content: self.message  
+    end
+
+    list
+  end
 end
--- spec/decorators/list_decorator_spec.rb
+++ spec/decorators/list_decorator_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe ListDecorator do
 
   it "delegates method calls to list argument" do
     list = double(list)
-    allow(list).to receive(:close)
+    allow(list).to receive(:open?)
 
-    ListDecorator.new(list).close
+    ListDecorator.new(list).open?
 
-    expect(list).to have_received(:close)
+    expect(list).to have_received(:open?)
   end
 
   it "prepares message to send to actioncable" do
@@ -21,5 +21,17 @@ RSpec.describe ListDecorator do
 
     expect(ListDecorator.new(list).message).to eq("A lista de tarefas \"titulo\" foi atualizada.")
   end
-  it "broadcasts message to actioncable"
+
+  it "broadcasts message when close method is called" do
+    list = double(list, title: 'titulo')
+    subject = ListDecorator.new(list)
+
+    allow(list).to receive(:bookmark_user_ids).and_return([1])
+    allow(list).to receive(:close).and_return(list)
+    allow(ActionCable.server).to receive(:broadcast)
+
+    subject.close
+
+    expect(ActionCable.server).to have_received(:broadcast)
+  end
 end
