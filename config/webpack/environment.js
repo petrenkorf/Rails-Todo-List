bc76ef9abb8199b0d3ae1b309c3deb1a1d1f7765
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2020-08-06 02:48:17 -0300

    Bootstrap adicionado

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,6 +8,7 @@ environment.plugins.append(
     'Provide',
     new webpack.ProvidePlugin({
         $: 'jquery',
-        jQuery: 'jquery'
+        jQuery: 'jquery',
+        Popper: ['popper.js', 'default']
     })
 )
